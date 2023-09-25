94552a0b570cc84ea0f0349d8cf0ed8e1bff4735
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-09-25 14:02:53 +0200

    update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,6 @@
 import {useState} from "react";
 import {Form,Label,Button,Input,Section} from "./Form.styled"
 import { nanoid } from 'nanoid';
-import PropTypes from "prop-types"
 
 
 const ContactForm = ({onAddContact}) => {
